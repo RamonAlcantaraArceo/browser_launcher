43f1c98b7444c3c3b5a0bc1e444c5096b18ddfa5
Ramon Alcantara Arceo <ramonalcantaraarceo@mini.local>
2026-02-11 15:13:43 -0800

    Missing base cases

--- tests/browsers/test_chrome.py
+++ tests/browsers/test_chrome.py
@@ -56,7 +56,21 @@ class TestChromeBrowserName:
         chrome_instance._driver = None
 
         chrome_instance.safe_get_address("about:blank")
-        pass
+        chrome_instance.logger.error.assert_any_call(
+            "No driver instance available for navigation."
+        )
+
+    def test_has_driver_fails_to_get_safe_url(self, chrome_instance: BrowserLauncher):
+        chrome_instance.launch("about:blank")
+        assert chrome_instance.driver
+
+        chrome_instance._driver.get = mock.Mock(
+            side_effect=Exception("Navigation failed")
+        )
+        chrome_instance.safe_get_address("about:blank")
+        chrome_instance.logger.error.assert_any_call(
+            "Failed to navigate to about:blank: Navigation failed", exc_info=True
+        )
 
     def test_failed_to_launch(self, chrome_instance: BrowserLauncher):
         """Test that mocks the webdriver.Chrome constructor and raises a
